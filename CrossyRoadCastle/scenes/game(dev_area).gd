490c8fe8d1d86f3bb96ebba7c550add548344333
javidwaldron <javidwaldron@gmail.com>
2025-02-20 11:24:19 -0500

    Fixed for real
    I dont know what happened but this is essentially back to how it was before last class. Gonna attempt to redo the multiplayer merge in a bit

--- CrossyRoadCastle/scenes/game(dev_area).gd
+++ CrossyRoadCastle/scenes/game(dev_area).gd
@@ -1,7 +1,30 @@
 extends Node2D
 
-var Player = player.new()
+@onready var player: player = $Player
+@onready var player_2: player = $Player2
+
 # Called when the node enters the scene tree for the first time.
 func _process(delta):
-	print(Player.name)
+	pass
 	
+func _ready():
+	_the_setup()
+		
+
+
+func _the_setup():
+	if Global.isPlayer1active == true:
+		$Player.visible = true
+		$Player.playerChoice = Global.player1Choice
+
+	if Global.isPlayer2active == true:
+		$Player2.visible = true
+		$Player2.playerChoice = Global.player2Choice
+		
+	if Global.isPlayer3active == true:
+		$Player3.visible = true
+		$Player3.playerChoice = Global.player3Choice
+		
+	if Global.isPlayer4active == true:
+		$Player4.visible = true
+		$Player4.playerChoice = Global.player4Choice
