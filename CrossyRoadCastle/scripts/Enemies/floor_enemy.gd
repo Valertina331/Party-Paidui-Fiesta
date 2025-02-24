b19cf60068d32ea88e1c7004bcdecc61f34e5dcf
javidwaldron <javidwaldron@gmail.com>
2025-02-24 13:06:00 -0500

    will fix back
    merging issue

--- CrossyRoadCastle/scripts/Enemies/floor_enemy.gd
+++ CrossyRoadCastle/scripts/Enemies/floor_enemy.gd
@@ -3,6 +3,7 @@ class_name FloorEnemy
 
 @export var enemy_health := 1 
 @export var head_detection: Area2D 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
 	head_detection.body_entered.connect(_on_head_touched)
@@ -11,12 +12,12 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 	if velocity.length() > 0:
-		$Sprite.play("default")
+		$AnimatedSprite2D.play("default")
 		
 	if velocity.x > 0:
-		$Sprite.flip_h = false
+		$AnimatedSprite2D.flip_h = false
 	else:
-		$Sprite.flip_h = true
+		$AnimatedSprite2D.flip_h = true
 
 func _on_head_touched(body):
 	if body.is_in_group("Player"): 
