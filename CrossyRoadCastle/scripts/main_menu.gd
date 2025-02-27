f610df36cee14f57d8096de4386e8048002999df
xiaoweiI <41134493+xiaoweiI@users.noreply.github.com>
2025-02-26 20:30:01 -0500

    fixed1

--- CrossyRoadCastle/scripts/main_menu.gd
+++ CrossyRoadCastle/scripts/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 #I know this is super repetitive but it kept getting a bug I couldnt figure out how to solve
-const MAX_CHARACTER_INDEX = 2 #max number of characters
+const MAX_CHARACTER_INDEX = 4 #max number of characters
 
 
 var player_choices = {
@@ -11,29 +11,15 @@ var player_choices = {
 	"Player3": 0,
 	"Player4": 0
 }
+var character_images = [
+	preload("res://assets/CharacterSheets/AngryCatWalkSheet.png"),
+	preload("res://assets/CharacterSheets/chocolate strawberry.png"),
+	preload("res://assets/CharacterSheets/FishWithLegsWalkSheet.png"),
+	preload("res://assets/CharacterSheets/Pixelcharactersheet.png"),
+	preload("res://assets/CharacterSheets/rabit2.png"),
+	preload("res://assets/CharacterSheets/RubberDuckyScarWalkSheet.png")
+]
 
-@onready var players = {
-	"Player1": {
-		"sprite": $Player1/Sprite,
-		"left_button": $Player1/P1LeftArrow,
-		"right_button": $Player1/P1RightArrow
-	},
-	"Player2": {
-		"sprite": $Player2/Sprite,
-		"left_button": $Player2/P2LeftArrow,
-		"right_button": $Player2/P2RightArrow
-	},
-	"Player3": {
-		"sprite": $Player3/Sprite,
-		"left_button": $Player3/P3LeftArrow,
-		"right_button": $Player3/P3RightArrow
-	},
-	"Player4": {
-		"sprite": $Player4/Sprite,
-		"left_button": $Player4/P4LeftArrow,
-		"right_button": $Player4/P4RightArrow
-	}
-}
 
 @onready var not_joined_labels = {
 	"Player2": $NotJoined2,
@@ -48,7 +34,36 @@ var active_players = {
 	"Player3": false,
 	"Player4": false
 }
+var players = {}  
+
 func _ready():
+	players = {
+		"Player1": {
+			"sprite": $Player1/Sprite,
+			"left_button": $Player1/P1LeftArrow,
+			"right_button": $Player1/P1RightArrow
+		},
+		"Player2": {
+			"sprite": $Player2/Sprite,
+			"left_button": $Player2/P2LeftArrow,
+			"right_button": $Player2/P2RightArrow
+		},
+		"Player3": {
+			"sprite": $Player3/Sprite,
+			"left_button": $Player3/P3LeftArrow,
+			"right_button": $Player3/P3RightArrow
+		},
+		"Player4": {
+			"sprite": $Player4/Sprite,
+			"left_button": $Player4/P4LeftArrow,
+			"right_button": $Player4/P4RightArrow
+		}
+	}
+	not_joined_labels = {
+		"Player2": $NotJoined2,
+		"Player3": $NotJoined3,
+		"Player4": $NotJoined4
+	}
 	for player in players.keys():
 		players[player]["left_button"].pressed.connect(func(): change_character(player, -1))
 		players[player]["right_button"].pressed.connect(func(): change_character(player, 1))
@@ -61,7 +76,8 @@ func change_character(player, direction):
 func update_ui():
 	for player in players.keys():
 		players[player]["sprite"].frame = player_choices[player]
-		not_joined_labels[player].visible = not active_players[player]
+		if player in not_joined_labels:  # Check if the player has a not_joined label
+			not_joined_labels[player].visible = not active_players[player]
 		players[player]["sprite"].visible = active_players[player]
 		
 func _on_play_pressed():
