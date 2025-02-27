e069ebae396d2b05d07b314a7d4f6b37532a4e8c
xiaoweiI <41134493+xiaoweiI@users.noreply.github.com>
2025-02-27 16:33:11 -0500

    1

--- CrossyRoadCastle/scripts/main_menu.gd
+++ CrossyRoadCastle/scripts/main_menu.gd
@@ -2,96 +2,50 @@ extends Control
 
 
 #I know this is super repetitive but it kept getting a bug I couldnt figure out how to solve
-const MAX_CHARACTER_INDEX = 4 #max number of characters
+@onready var player_containers: GridContainer = $PlayerContainers
+const PLAYER_SELECT = preload("res://scenes/player_select.tscn")
 
+var maxvalue = 2
 
-var player_choices = {
-	"Player1": 0,
-	"Player2": 0,
-	"Player3": 0,
-	"Player4": 0
-}
-var character_images = [
-	preload("res://assets/CharacterSheets/AngryCatWalkSheet.png"),
-	preload("res://assets/CharacterSheets/chocolate strawberry.png"),
-	preload("res://assets/CharacterSheets/FishWithLegsWalkSheet.png"),
-	preload("res://assets/CharacterSheets/Pixelcharactersheet.png"),
-	preload("res://assets/CharacterSheets/rabit2.png"),
-	preload("res://assets/CharacterSheets/RubberDuckyScarWalkSheet.png")
-]
+var device: int #This is to be stored in the player so the individual controllers can change the character
+var playersPlaying = [] #This is necessary just for gathering players together, but true value is stored in dictionary
+var devicesin = [] #This stops the repeating of devices
 
-
-@onready var not_joined_labels = {
-	"Player2": $NotJoined2,
-	"Player3": $NotJoined3,
-	"Player4": $NotJoined4
-}
-@onready var play_button = $PlayButton
-
-var active_players = {
-	"Player1": true,
-	"Player2": false,
-	"Player3": false,
-	"Player4": false
-}
-var players = {}  
-
-func _ready():
-	players = {
-		"Player1": {
-			"sprite": $Player1/Sprite,
-			"left_button": $Player1/P1LeftArrow,
-			"right_button": $Player1/P1RightArrow
-		},
-		"Player2": {
-			"sprite": $Player2/Sprite,
-			"left_button": $Player2/P2LeftArrow,
-			"right_button": $Player2/P2RightArrow
-		},
-		"Player3": {
-			"sprite": $Player3/Sprite,
-			"left_button": $Player3/P3LeftArrow,
-			"right_button": $Player3/P3RightArrow
-		},
-		"Player4": {
-			"sprite": $Player4/Sprite,
-			"left_button": $Player4/P4LeftArrow,
-			"right_button": $Player4/P4RightArrow
-		}
-	}
-	not_joined_labels = {
-		"Player2": $NotJoined2,
-		"Player3": $NotJoined3,
-		"Player4": $NotJoined4
-	}
-	for player in players.keys():
-		players[player]["left_button"].pressed.connect(func(): change_character(player, -1))
-		players[player]["right_button"].pressed.connect(func(): change_character(player, 1))
-	play_button.pressed.connect(_on_play_pressed)
-	update_ui()
+#Function to get all available playable devices on computer
+func get_unjoined_devices():
+	var devices = Input.get_connected_joypads()
+	devices.append(-1)
 	
-func change_character(player, direction):
-	player_choices[player] = (player_choices[player] + direction) % (MAX_CHARACTER_INDEX + 1)
-	update_ui()
-func update_ui():
-	for player in players.keys():
-		players[player]["sprite"].frame = player_choices[player]
-		if player in not_joined_labels:  # Check if the player has a not_joined label
-			not_joined_labels[player].visible = not active_players[player]
-		players[player]["sprite"].visible = active_players[player]
+	return devices
+
+func _process(fixed):
+	_multiplayer_setup()
+	get_unjoined_devices()
+	
+#Start button logic, can create safegaurd for everyone to say ready first
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://javidtowerlevels/1.tscn")
+	
+
+#Essentially takes all the devices and the moment someone hits A, or enter will add them as a player, assuming the device hasnt already been used
+func _multiplayer_setup():
+	for i in get_unjoined_devices():
+		if MultiplayerInput.is_action_just_pressed(i, "jump"):
+			if !devicesin.has(i):
+				devicesin.append(i)
+				playerjoin(i)
 		
-func _on_play_pressed():
-	print("final character:", player_choices)
-	get_tree().change_scene_to_file("res://scenes/game(dev_area).tscn")
-	
-func toggle_player_active(player):
-	active_players[player] = !active_players[player]
-	update_ui()
+				
 
-func _input(event):
-	if event.is_action_pressed("Testdebug2"):
-		toggle_player_active("Player2")
-	if event.is_action_pressed("Testdebug3"):
-		toggle_player_active("Player3")
-	if event.is_action_pressed("Testdebug4"):
-		toggle_player_active("Player4")
+#This is saying hey, if the player size isnt 4, create a player, add it to the people playing, give it this value and placement and add it visually to the screen
+func playerjoin(device):
+	if playersPlaying.size() !=4 :
+		var player = PLAYER_SELECT.instantiate()
+		playersPlaying.append(player)
+		for players in playersPlaying:
+			player.playerNumber = playersPlaying.size()
+			player.device = device
+			player.characterChoice = 0
+			player_containers.add_child(player)
+			Global.add_to_dict(str(player.playerNumber), player.characterChoice, player.playerNumber, player.device)
+			
--- CrossyRoadCastle/scripts/player.gd
+++ CrossyRoadCastle/scripts/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 class_name player
 
+const death_Speed = -100
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -9,43 +10,79 @@ var device : int
 var playerNumber: int
 var characterChoice: int
 
+var is_dead = false
+var fall = false
 
+
+@onready var death_pause: Timer = $DeathPause
+@onready var dead_exclaim: TextureRect = $DeadExclaim
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @export var bounce_velocity := -300
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+
+func _physics_process(delta):
+	
+	#For testing delete laTER
+	#if MultiplayerInput.is_action_just_pressed(device,"cancel"):
+		#is_dead = true
+	
+	
+	
+	if is_dead == false:
+	
+		# Add the gravity.
+		if not is_on_floor():
+			velocity += get_gravity() * delta
 
 	# Handle jump.
-	if MultiplayerInput.is_action_just_pressed(device,"jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		if MultiplayerInput.is_action_just_pressed(device,"jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := MultiplayerInput.get_axis(device,"move_left", "move_right")
+		var direction := MultiplayerInput.get_axis(device,"move_left", "move_right")
 	
-	if direction> 0:
-		animated_sprite_2d.flip_h = false
-	elif direction < 0:
-		animated_sprite_2d.flip_h = true
+		if direction> 0:
+			animated_sprite_2d.flip_h = false
+		elif direction < 0:
+			animated_sprite_2d.flip_h = true
 	
-	if direction == 0:
-		animated_sprite_2d.play(str(characterChoice)+ "idle")
-	else:
-		animated_sprite_2d.play(str(characterChoice)+"walking")
+		if direction == 0:
+			animated_sprite_2d.play(str(characterChoice)+ "idle")
+		else:
+			animated_sprite_2d.play(str(characterChoice)+"walking")
 	
 	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+		move_and_slide()
+		
+	if is_dead == true && dead_exclaim.visible == false:
+		dead()
+		
+	if fall == true:
+		position.y -= death_Speed * delta
+		dead_exclaim.visible = false
 	
 #bounce when step on enemies
 func bounce():
 	velocity.y = bounce_velocity
 	move_and_slide()
+	
+#These two lead to the death process, removes instance of player
+func dead():
+	position = position
+	dead_exclaim.visible = true
+	collision_shape_2d.disabled = true
+	death_pause.start()
+
+func _on_death_pause_timeout():
+	animation_player.play("death")
+	fall = true
+	
