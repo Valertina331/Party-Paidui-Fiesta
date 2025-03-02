ba736166fca41b2759e3ad68ab883261d606374d
xiaoweiI <41134493+xiaoweiI@users.noreply.github.com>
2025-03-02 16:02:39 -0500

    1

--- CrossyRoadCastle/scripts/game(dev_area).gd
+++ CrossyRoadCastle/scripts/game(dev_area).gd
@@ -1,18 +1,68 @@
 extends Node2D
 
 var playersPlaying = []
+
 const player = preload("res://scenes/player.tscn")
 
+var currentYCoins : int
+var currentPCoins : int
+var floorsClimbed : int
+var currentHearts : int
+
+#Will put on export in inspector string can be modified
+var tower_to_call = Global.tower_to_call
+var typePrefix = ".tscn"
+
+var totalplayers : int
+var alldead = false
+var levelpass = false
+var startNextTimer = false
+
+@onready var load_next_timer: Timer = $LoadNextTimer
+
+#These have to be set in the inspect, make the heart on the right the first element
 @export var spawnlocations: Array[Marker2D] = []
+@export var hearticons: Array[Control] = []
+
+#How players are instantiated onto the scene
 @onready var game_dev_area_: Node2D = $"."
 
-# Called when the node enters the scene tree for the first time.
+
+@onready var heart_containers: GridContainer = $LevelUI/HeartContainer
+@onready var yellow_coin_amount: Label = $LevelUI/CoinLabels/YellowCoinAmount
+@onready var purple_coin_amount: Label = $LevelUI/CoinLabels/PurpleCoinAmount
+@onready var floortext: Label = $LevelUI/FloorLevel/Floortext
 
 
+#Gets players from Global and increments floor level by one
+#Grabs the door from Doortoadvance and connects to its signal
 func _ready():
 	_getplayers()
+	var next_level_door = $DoorToAdvance/Door
+	next_level_door.connect("levelpassed", Callable(self, "_on_level_passed"))
 	
+	
+	
+	
+func _process(delta):
+	_get_current_players()
+	_getlabelinfo()
+	
+	#All dead triggered by update/process, remove heart is not flipped by anything else other than this call so it wont repeat
+	if alldead == true && levelpass == false:
+		Global.change_health(-1)
+		get_tree().reload_current_scene()
+		
+		
+	if alldead == true and levelpass == true:
+		Global.add_to_floor_climbed(1)
+		get_tree().change_scene_to_file(tower_to_call+str(Global.get_levels_climbed()+1)+typePrefix)
+		
+	if alldead == false && levelpass == true && startNextTimer == false:
+		load_next_timer.start(3)
+		startNextTimer = true
 
+#Takes the dictionary defined in Global from the main menu and uses it to program instances of players
 func _getplayers():
 		for players in Global.playersPlaying:
 			var levelplayer = player.instantiate()
@@ -20,6 +70,48 @@ func _getplayers():
 			levelplayer.device = playerdata.get("device")
 			levelplayer.characterChoice = playerdata.get("characterChoice")
 			levelplayer.playerNumber = playerdata.get("playerNumber")
-			levelplayer.position = spawnlocations[levelplayer.playerNumber].position
+			levelplayer.position = spawnlocations[levelplayer.playerNumber].global_position
 			game_dev_area_.add_child(levelplayer)
-			
+		
+		
+
+func _getlabelinfo():
+	#Each of these uses a method to properly overwrite local integers with the global integer
+	currentYCoins = Global.get_current_yellow_coins()
+	currentPCoins = Global.get_current_purple_coins()
+	floorsClimbed = Global.get_levels_climbed()
+	currentHearts = Global.get_current_health()
+	
+	yellow_coin_amount.text = str(currentYCoins)
+	purple_coin_amount.text = str(currentPCoins)
+	floortext.text = str(floorsClimbed)
+	
+	#Method I developed for my last game should not need to be touched, essentially if the amount of hearts is equal
+	# to the current health stay red, if not turn grey
+	if currentHearts != 0:
+		hearticons[currentHearts-1]._full_heart()
+		hearticons[0]._full_heart()
+		for i in hearticons:
+			if hearticons.size() == currentHearts:
+				i._full_heart()
+			if hearticons.size() > currentHearts:
+				hearticons[currentHearts]._lost_heart()
+	else:
+		hearticons[0]._lost_heart()
+
+#Gets all the current players, only once everyones dead does it remove the heart
+func _get_current_players():
+	var totalplayers = get_tree().get_nodes_in_group("Player").size()
+	if totalplayers == 0:
+			alldead = true
+
+#Will add code here to start timer, if all players arent destroyed advance to next stage
+func _on_level_passed():
+	levelpass = true
+	
+
+#Will replace with swirling animation like in real game
+func _on_load_next_timer_timeout():
+	var totalplayers = get_tree().get_nodes_in_group("Player")
+	for players in totalplayers:
+			players.queue_free()
--- CrossyRoadCastle/scripts/main_menu.gd
+++ CrossyRoadCastle/scripts/main_menu.gd
@@ -1,31 +1,43 @@
 extends Control
 
-
 #I know this is super repetitive but it kept getting a bug I couldnt figure out how to solve
 @onready var player_containers: GridContainer = $PlayerContainers
+@onready var javid_tower_0: AnimatedSprite2D = $PanelContainer/TextureRect/JavidTower0
+@onready var valentina_tower_1: AnimatedSprite2D = $PanelContainer/TextureRect/ValentinaTower1
+@onready var xiaowei_tower_2: AnimatedSprite2D = $PanelContainer/TextureRect/XiaoweiTower2
+
 const PLAYER_SELECT = preload("res://scenes/player_select.tscn")
 
-var maxvalue = 2
-
+var towerSelectedint = 0
 var device: int #This is to be stored in the player so the individual controllers can change the character
 var playersPlaying = [] #This is necessary just for gathering players together, but true value is stored in dictionary
 var devicesin = [] #This stops the repeating of devices
+var towersavailable = []
+
+func _ready():
+	towersavailable.append_array([javid_tower_0, valentina_tower_1, xiaowei_tower_2])
+
+
 
 #Function to get all available playable devices on computer
 func get_unjoined_devices():
 	var devices = Input.get_connected_joypads()
 	devices.append(-1)
-	
 	return devices
+
 
 func _process(fixed):
 	_multiplayer_setup()
 	get_unjoined_devices()
+	tower_animation()
 	
 #Start button logic, can create safegaurd for everyone to say ready first
 func _on_play_button_pressed():
-	get_tree().change_scene_to_file("res://javidtowerlevels/1.tscn")
+	var destination = Global.tower_Choice(towerSelectedint)
+	var prefix = Global.typePrefix
+	get_tree().change_scene_to_file(destination+"1"+prefix)
 	
+
 
 #Essentially takes all the devices and the moment someone hits A, or enter will add them as a player, assuming the device hasnt already been used
 func _multiplayer_setup():
@@ -34,18 +46,89 @@ func _multiplayer_setup():
 			if !devicesin.has(i):
 				devicesin.append(i)
 				playerjoin(i)
-		
-				
+		if MultiplayerInput.is_action_just_pressed(i, "start"):
+			if devicesin.has(i):
+				_on_play_button_pressed()
 
 #This is saying hey, if the player size isnt 4, create a player, add it to the people playing, give it this value and placement and add it visually to the screen
 func playerjoin(device):
-	if playersPlaying.size() !=4 :
+	if playersPlaying.size() <4 :
 		var player = PLAYER_SELECT.instantiate()
 		playersPlaying.append(player)
-		for players in playersPlaying:
-			player.playerNumber = playersPlaying.size()
-			player.device = device
-			player.characterChoice = 0
-			player_containers.add_child(player)
-			Global.add_to_dict(str(player.playerNumber), player.characterChoice, player.playerNumber, player.device)
+		
+		var available_number = 1
+		var taken_numbers = playersPlaying.map(func(p): return p.playerNumber)
+		while available_number in taken_numbers:
+			available_number +=1
+		
+		player.playerNumber = available_number
+		player.device = device
+		player.characterChoice = 0
+		
+		player_containers.add_child(player)
+		player.connect("imout", Callable(self, "_on_im_out"))
 			
+		Global.add_to_dict(str(player.playerNumber), player.characterChoice, player.playerNumber, player.device)
+		print(Global.playersPlaying)
+			
+
+#When this signal is detected removes player and device from arrays then updates number in code
+func _on_im_out(value):
+		var player_to_remove = null
+		for player in playersPlaying:
+			if player.device == value:
+				player_to_remove = player
+				break
+				
+		if player_to_remove:
+			player_to_remove.queue_free()
+			playersPlaying.erase(player_to_remove)
+			devicesin.erase(value)
+			
+			
+		var key_to_remove = null
+		for key in Global.playersPlaying.keys():
+			if Global.playersPlaying[key]["device"] == value:
+				key_to_remove = key
+				break
+		if key_to_remove:
+			Global.remove_entry(key_to_remove)
+			_update_player_numbers()
+		
+
+#This is for updating UI needed chatgpt help
+func _update_player_numbers():
+	for i in range(playersPlaying.size()):
+		var new_number = i + 1
+		playersPlaying[i].playerNumber = new_number
+		playersPlaying[i]._update_UI(new_number)
+		Global.playersPlaying[str(new_number)] = {
+		"characterChoice": playersPlaying[i].characterChoice,
+		"device": playersPlaying[i].device,
+		"playerNumber": new_number
+		}
+
+func tower_animation():
+	for i in towersavailable.size():
+		if i != towerSelectedint:
+			towersavailable[i].play("Unselected")
+		else:
+			towersavailable[i].play("Selected")
+		
+		
+
+
+func _on_tower_button_right_pressed():
+	print(towerSelectedint)
+	if towerSelectedint >= 2:
+		towerSelectedint = 0
+	else:
+		towerSelectedint +=1
+		
+
+func _on_tower_button_left_pressed():
+	print(towerSelectedint)
+	if towerSelectedint <= 0:
+		towerSelectedint = 2
+	else:
+		towerSelectedint -=1
