0f041e9a89bdfcc52e1a1061b939e719be4e88ad
javidwaldron <javidwaldron@gmail.com>
2025-03-17 22:25:25 -0400

    Fixed Enemy Glitch
    Disables colliders once hit and given bounce. Also added a method for game dev. Essentially if you want to animate characters to go their own way, like indiivudally flying in a direction with an animation player, just name the animation movement and it will play

--- CrossyRoadCastle/scripts/Enemies/enemy.gd
+++ CrossyRoadCastle/scripts/Enemies/enemy.gd
@@ -3,14 +3,19 @@ class_name Enemy
 
 @export var enemy_health := 1 
 @export var head_detection: Area2D 
+@export var body_detection: Area2D
 
 const death_Speed = -100
 var fall = false
 var collider
+var bodycollider
+var headcollider
 
 func _ready() -> void:
 	head_detection.body_entered.connect(_on_head_touched)
 	collider = $CollisionShape2D
+	bodycollider = $HeadDetection/CollisionShape2D
+	headcollider = $BodyDetection/CollisionShape2D
 	
 	if head_detection == null:
 		print("ERROR: head_detection is missing in", self.name)
@@ -36,16 +41,26 @@ func _on_head_touched(body):
 	if body.is_in_group("Player"):
 		print("Player detected!")
 		if body.global_position.y < global_position.y:
-			body.bounce()
-			print("Enemy killed by head stomp!")
-			$AnimationPlayer.play("dead")
-			fall = true
-			collider.set_deferred("disabled", true)
-		else:
-			body.is_dead = true
+			if fall == false:
+				body.bounce()
+				print("Enemy killed by head stomp!")
+				$AnimationPlayer.play("dead")
+				fall = true
+				_all_of_you_get_lost()
+		
 		
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		print("Player hit the enemy, dies!")
-		body.is_dead = true
+		if fall == false:
+			print("Player hit the enemy, dies!")
+			body.is_dead = true
+		
+func _all_of_you_get_lost():
+	collider.disabled = true
+	bodycollider.disabled =true
+	headcollider.disabled = true
+	head_detection.monitoring = false
+	head_detection.monitorable = false
+	body_detection.monitoring = false
+	body_detection.monitorable = false
